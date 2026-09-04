0dbe098e43c685b3a0faf14318ff9fc70df2ca9e
tobiasprocka16-bit <tobias.procka16@gmail.com>
2026-09-04 09:41:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+print("ako sa volas")
+
+meno = input()
+print(f"Ahoj {meno}")
+
+print("Kolko merias")
+height = int(input())
+
+if height > 150:
+    print("Mozes ist na kolotoc.")
+else:
+    print("nemozes ist na kolotoc")
